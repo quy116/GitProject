51d6fdeac2cb0896e2e5522e939e169756e1c4ac
quy116 <110885361+quy116@users.noreply.github.com>
2023-02-09 11:05:47 +0700

    Add files via upload

---
+++ GitProject-main/luyentap.js
@@ -0,0 +1,11 @@
+
+// Bài 1: Nhập vào hai số a và b, và kiểm tra xem a có chia hết cho b hay không
+
+var a = 10
+var b = 2
+var c = a / b
+if (c === 5) {
+    console.log("a chia het")
+} else {
+    console.log("khong chia dc ")
+}
